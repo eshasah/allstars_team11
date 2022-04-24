feaa9829f18db3f9cc896ec195d58c007cb42c65
eshasah <esha8sah@gmail.com>
2022-04-24 16:12:47 -0700

    architecture diagram

--- backend/app.js
+++ backend/app.js
@@ -9,15 +9,15 @@ app.use(express.json());
 const pool = require("./databaseConnection");
 
 const userRouter = require("./routes/userRoutes");
-const adminRouter = require("./routes/adminRoutes");
-const applicationRouter = require("./routes/applicationRoutes");
+//const adminRouter = require("./routes/adminRoutes");
+//const applicationRouter = require("./routes/applicationRoutes");
 const doseRouter = require("./routes/doseRoutes");
-const testRouter = require("./routes/testRoutes");
+//const testRouter = require("./routes/testRoutes");
 
-app.use("/api/v1/admins", adminRouter);
+//app.use("/api/v1/admins", adminRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/applications", applicationRouter);
+//app.use("/api/v1/applications", applicationRouter);
 app.use("/api/v1/doses", doseRouter);
-app.use("/api/v1/testing", testRouter);
+//app.use("/api/v1/testing", testRouter);
 
 module.exports = app;
