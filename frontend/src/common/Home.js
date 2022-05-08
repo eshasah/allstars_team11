8881511c305aff09e0f3b3fd2aaaf26364c3e851
suresh-mula <89316778+suresh-mula@users.noreply.github.com>
2022-05-08 12:34:22 -0700

    Home Page

--- frontend/src/common/Home.js
+++ frontend/src/common/Home.js
@@ -1,11 +1,171 @@
 import React from "react";
-export default function Home(props) {
-    const user_id = localStorage.getItem("user_id")
+import { Button, Modal, Dropdown } from 'react-bootstrap';
+
+class Home extends React.Component {
+  state = {
+    getCities: [],
+    getSlots: ["11.00 - 12:00 P.M", "2.00 - 4.00 P.M", "4.00 - 6.00 P.M"],
+    isOpen: false,
+    isBooking: false,
+    isVaccineHistory: false,
+    isCitiesAvailble: false,
+  }
+
+  closeModal = () => this.setState({ isOpen: false });
+  openModal = () => this.setState({ isOpen: true });
+
+  bookSlot = () => this.setState({
+    isOpen: true
+  });
+
+  createBooking = () => this.setState({ isBooking: true, isVaccineHistory: false });
+  getVaccineHistory = () => this.setState({ isBooking: false, isVaccineHistory: true, isCitiesAvailble: false });
+
+  bookNow = () => {
+    this.setState({ isOpen: false });
+    alert('Booked Successfully!!!!')
+  }
+  getCitiesByPincode = (event) => {
+    this.setState({
+      isCitiesAvailble: true,
+      getCities: ["Mancherial", "OldMancherial", "Luxettipet", "Srirampur", "Shirkey"]
+    })
+  }
+  render() {
+
+    return (
+      <><div>
+        <br></br>
+        <div className="container">
+
+          <div id="demo" className="carousel slide" data-ride="carousel">
+
+            <ul className="carousel-indicators">
+              <li data-target="#demo" data-slide-to="0" className="active"></li>
+              <li data-target="#demo" data-slide-to="1"></li>
+              <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
+
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src="https://www.dqindia.com/wp-content/uploads/2020/03/corona-4916954_640.jpg" alt="Los Angeles" width="1100" height="600px" margin="10px" />
+              </div>
+              <div className="carousel-item">
+                <img src="https://c.ndtvimg.com/2020-06/k5890kk8_covid19-vaccine-generic-reuters-650_625x300_10_June_20.jpg?im=Resize=(1230,900)" alt="Chicago" width="1100" height="600" margin="10px" />
+              </div>
+              <div className="carousel-item">
+                <img src="https://i0.wp.com/www.smartprix.com/bytes/wp-content/uploads/2021/02/aarogya.png?fit=1200%2C630&ssl=1" alt="New York" width="1100" height="600px" margin="10px" />
+              </div>
+            </div>
+
+            <a className="carousel-control-prev" href="#demo" data-slide="prev">
+              <span className="carousel-control-prev-icon"></span>
+            </a>
+            <a className="carousel-control-next" href="#demo" data-slide="next">
+              <span className="carousel-control-next-icon"></span>
+            </a>
+          </div>
+
+        </div>
+        <div className="text-center"><br></br>
+
+          <button type="button" className="btn btn-info"  onClick={this.createBooking}>Book Vaccine</button>&nbsp;&nbsp;&nbsp;
+          <button type="button" className="btn btn-info"  onClick={this.getVaccineHistory}>Vaccine History</button><br></br><br></br>
+         
+
+          {
+            this.state.isVaccineHistory ?
+              <div className="container">
+                <table class="table">
+                  <thead class="thead-dark">
+                    <tr>
+                      <th scope="col">Dose</th>
+                      <th scope="col">Vaccine Name</th>
+                      <th scope="col">Reference Id</th>
+                      <th scope="col">Date of Vaccination</th>
+                      <th scope="col">Registered Mobile Number</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Covaccine</td>
+                      <td>123</td>
+                      <td>12/03/2022</td>
+                      <td>9999999999</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Covaccine</td>
+                      <td>321</td>
+                      <td>12/06/2022</td>
+                      <td>8888888888</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+              </div>
+              :  <div></div>
+          }
+
+          {
+            this.state.isBooking ? 
+            <><div>
+                <input type="number" placeholder="Please Enter Pincode" className="form-contro input-fieldl" />&nbsp;&nbsp;
+                <button type="button" className="btn btn-success" onClick={this.getCitiesByPincode}>Search</button>
+                <br></br>
+              </div><br></br></> 
+            : <div></div>
+          }
+
+          {
+            this.state.isCitiesAvailble ?
+            <div className="container card-div">
+            <div className="card">
+              <div className="card-body">
+                <div>
+                  {this.state.getCities.map(city => {
+                    return <span>&nbsp;&nbsp;&nbsp;<button type="button" className="btn btn-secondary" onClick={this.bookSlot}>{city}</button></span>;
+
+                  })}
+                </div>
+              </div>
+            </div>
+
+          </div>
+          : <div></div>
+          }
+
+         
 
 
-  return (
-      <div>
-          This is homepage, welcome {user_id}!
+
+        </div>
       </div>
-  );
+
+        <Modal show={this.state.isOpen} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Please Select Slots</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="card">
+              <div className="card-body">
+                <div>
+                  {this.state.getSlots.map(city => {
+                    return <span>&nbsp;&nbsp;&nbsp;<button type="button" className="btn btn-secondary mt-1">{city}</button></span>;
+                  })}
+                </div>
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.bookNow}>
+              Book Now
+            </Button>
+          </Modal.Footer>
+        </Modal></>
+    );
+  }
 }
+
+export default Home;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { createRoot } from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      {/* <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"/>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+ 
+</head>  */}
+  <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
@@ -23,4 +33,4 @@ import App from './App';
 //     </section>
 //   );
 // }
-ReactDom.render(<App/>,document.getElementById('root'));
+// ReactDom.render(<App/>,document.getElementById('root'));
