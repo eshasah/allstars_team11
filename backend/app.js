ce3f3587149d787a451cd1becb2699c906e587fa
Gayatri Yadkikar <yadkikargayatri@users.noreply.github.com>
2022-05-08 19:18:53 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,13 @@ const adminRouter = require("./routes/adminRoutes");
 const applicationRouter = require("./routes/applicationRoutes");
 const doseRouter = require("./routes/doseRoutes");
 //const testRouter = require("./routes/testRoutes");
+const slotRouter = require("./routes/slotRouter");
 
 //app.use("/api/v1/admins", adminRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/applications", applicationRouter);
 app.use("/api/v1/doses", doseRouter);
 //app.use("/api/v1/testing", testRouter);
+app.use("/api/v1/slot", slotRouter);
 
 module.exports = app;
