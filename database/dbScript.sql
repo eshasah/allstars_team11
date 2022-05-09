2db8e3d6483bbe9cceefe5d690cc27eb34711e7b
eshasah <esha8sah@gmail.com>
2022-05-08 17:20:40 -0700

    appointment table updated

--- database/dbScript.sql
+++ database/dbScript.sql
@@ -21,7 +21,7 @@ CREATE TABLE Vaccine_Recipient (
 );
 ALTER TABLE vaccine_recipient AUTO_INCREMENT=100;
 
-CREATE TABLE Dose (
+CREATE TABLE dose (
     dose_id int NOT NULL AUTO_INCREMENT,
     dose_no varchar(255) NOT NULL,
     PRIMARY KEY (dose_id)
@@ -31,17 +31,33 @@ ALTER TABLE dose AUTO_INCREMENT=100;
 CREATE TABLE slot (
 	slot_id int NOT NULL AUTO_INCREMENT,
     start_time datetime NOT NULL,
+    street varchar(45) NOT NULL,
+    zip_code varchar(5) NOT NULL,
+    city varchar(45) NOT NULL,
+    state varchar(45) NOT NULL,
     PRIMARY KEY (slot_id)
 );
 ALTER TABLE slot AUTO_INCREMENT=100;
+
+CREATE TABLE available_slot(
+    slot_id int NOT NULL,
+    dose_id int NOT NULL,
+    numer_of_slots int DEFAULT 0,
+    PRIMARY KEY (slot_id, dose_id)
+    FOREIGN KEY (slot_id) REFERENCES slot(slot_id),
+    FOREIGN KEY (dose_id) REFERENCES dose(dose_id)
+)
 
 CREATE TABLE appointment (
 	apt_id int NOT NULL AUTO_INCREMENT,
     slot_id int NOT NULL,
     dose_id int NOT NULL,
+    user_id int NOT NULL,
+    apt_status varchar(15) NOT NULL,
     primary key(apt_id),
     FOREIGN KEY (slot_id) REFERENCES slot(slot_id),
-    FOREIGN KEY (dose_id) REFERENCES dose(dose_id)
+    FOREIGN KEY (dose_id) REFERENCES dose(dose_id),
+    FOREIGN KEY (user_id) REFERENCES vaccine_recipient(user_id)
 );
 ALTER TABLE appointment AUTO_INCREMENT=100;
 
