ec324aee1ba9e4c7dcfc94893edf64d638073afe
eshasah <esha8sah@gmail.com>
2022-05-12 17:04:12 -0700

    view history backend done

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,11 @@ const pool = require("./databaseConnection");
 const userRouter = require("./routes/userRoutes");
 const testRouter = require("./routes/testRoutes");
 const slotRouter = require("./routes/slotRouter");
+const historyRouter = require("./routes/historyRouter");
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/testing", testRouter);
 app.use("/api/v1/slot", slotRouter);
+app.use("/api/v1/history", historyRouter);
 
 module.exports = app;
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -170,34 +170,3 @@ exports.updateUser = (req, res) => {
     );
   });
 };
-
-
-exports.vaccineHistory = (req, res)=>{
-    pool.getConnection((error,connection) => {
-        console.log("get slot");
-      if (error) throw error;
-      console.log('Connection to database as ${connection.threadId}');
-      const user_id = req.query.user_id;
-      
-       if(user_id==null){
-         console.log('Enter a valid value');
-       }
-let searchQuery = "SELECT * FROM appointment app INNER JOIN vaccine_recipient User ON app.user_id = User.user_id where app.user_id = ?";
-       connection.query(
-        searchQuery,
-         [user_id],
-          (error, row) => {
-            connection.release();
-
-            if(!error) {
-              res.status(200).send(row);
-            }
-            else {
-              console.log(error);
-            }
-
-            }
-          
-         );
-    });
-  }
